6e611c4a71781feabef786465aad57e081ea8196
Adam Simo <adamsimo626@gmail.com>
2023-11-10 19:26:42 +0100

    testing IaC - problem with AWS

--- IaC/test.tf
+++ IaC/test.tf
@@ -1,8 +1,28 @@
+terraform {
+
+  required_providers {
+
+    aws = {
+
+      source  = "hashicorp/aws"
+
+      version = "~> 4.16"
+
+    }
+
+  }
+
+  required_version = ">= 1.2.0"
+
+}
+
 provider "aws" {
   profile = "default"
-  region  = "eu-west-1"
+  region  = "us-east-1"
 }
-resource "aws_instance" "example" {
-  ami           = "ami-0694d931cee176e7d"
-  instance_type = "t2.micro"
+
+
+resource "aws_instance" "example-ec2" {
+  ami           = "ami-0fc5d935ebf8bc3bc"
+  instance_type = "t2.nano"
 }
