72eeecd2bc98e40f1c93c3432fe810458ae5a4ac
Adam Simo <adamsimo626@gmail.com>
2023-11-12 19:48:36 +0100

    deployed ec2 instance

--- IaC/test.tf
+++ IaC/test.tf
@@ -18,11 +18,11 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-east-1"
+  region  = "eu-west-1"
 }
 
 
 resource "aws_instance" "example-ec2" {
-  ami           = "ami-0fc5d935ebf8bc3bc"
-  instance_type = "t2.nano"
+  ami           = "ami-0694d931cee176e7d"
+  instance_type = "t2.micro"
 }
